b48268f88912d8a2759bcabd1d1a9bea26824074
Yehezkiel-simbuang <simbuangyehezkiel@gmail.com>
2023-11-18 10:45:38 +0700

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://fragile-pea-coat-jay.cyclic.app",
+        target: "http://localhost:3000",
         secure: false,
       },
     },
