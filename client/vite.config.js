6b671a001172adb3153d8f84d49a947f60b70259
Yehezkiel-simbuang <simbuangyehezkiel@gmail.com>
2023-11-18 09:56:12 +0700

    for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://art-gallery-livid.vercel.app",
         changeOrigin: true,
       },
     },
